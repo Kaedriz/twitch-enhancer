6658d784b09237d2817e7bb4b16398f21094a6a2
igorovh <animekgaming@gmail.com>
2024-08-26 23:18:55 +0200

    changes

--- src/storage/local.storage.ts
+++ src/storage/local.storage.ts
@@ -1,21 +1,24 @@
 import Storage from "./storage.ts";
 
 export default class LocalStorage<Map> extends Storage<Map> {
-	private readonly storage: Partial<Record<keyof Map, Map[keyof Map]>>;
-
-	constructor(private readonly name: string) {
-		super();
-		this.storage = JSON.parse(localStorage.getItem(name) || "{}") as Partial<
-			Record<keyof Map, Map[keyof Map]>
-		>;
-	}
-
 	async get<K extends keyof Map>(key: K): Promise<Map[K] | undefined> {
+		if (!this.storage) return;
 		return this.storage[key] as Map[K] | undefined;
 	}
 
 	async set<K extends keyof Map>(key: K, value: Map[K]): Promise<void> {
+		if (!this.storage) return;
 		this.storage[key] = value;
+		await this.save();
+	}
+
+	protected async load() {
+		this.storage = JSON.parse(
+			localStorage.getItem(this.name) || "{}",
+		) as Partial<Record<keyof Map, Map[keyof Map]>>;
+	}
+
+	protected async save() {
 		localStorage.setItem(this.name, JSON.stringify(this.storage));
 	}
 }
--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -1,4 +1,10 @@
 export default class Storage<Map> {
+	protected storage: Partial<Record<keyof Map, Map[keyof Map]>> | undefined;
+
+	constructor(protected readonly name: string) {
+		this.load();
+	}
+
 	async get<K extends keyof Map>(key: K): Promise<Map[K] | undefined> {
 		throw new Error("Not implemented");
 	}
@@ -6,4 +12,8 @@ export default class Storage<Map> {
 	async set<K extends keyof Map>(key: K, value: Map[K]): Promise<void> {
 		throw new Error("Not implemented");
 	}
+
+	protected load() {}
+
+	protected async save() {}
 }
