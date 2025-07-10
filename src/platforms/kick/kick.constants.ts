9dfc7e7021c78c2eb31cacd43e70230e089473ef
igorovh <xoxo@igor.ovh>
2025-07-10 20:27:27 +0200

    fix

--- src/platforms/kick/kick.constants.ts
+++ src/platforms/kick/kick.constants.ts
@@ -4,7 +4,7 @@ export const KICK_DEFAULT_SETTINGS: KickSettings = {
 	chatImagesEnabled: false,
 	chatImagesOnHover: false,
 	chatImagesSize: 15,
-	quickAccessLinks: [{ title: "Streams Charts", url: "https://streamscharts.com/channels/furazek?platform=kick" }],
+	quickAccessLinks: [{ title: "Streams Charts", url: "https://streamscharts.com/channels/%username%?platform=kick" }],
 };
 
 export const KICK_LIVE_VIDEO_DURATION = 1073741824;
